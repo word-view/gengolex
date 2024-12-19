9ebe8f11a46b19a6c700443af587c25f24be58c4
Arthur Araujo <arthur.araujo@tutanota.com>
2024-12-19 13:17:30 -0300

    #1 Make words representable by default

--- src/main/kotlin/cc/wordview/gengolex/languages/Word.kt
+++ src/main/kotlin/cc/wordview/gengolex/languages/Word.kt
@@ -5,10 +5,10 @@ package cc.wordview.gengolex.languages
  *
  * @property parent Identifier used to associate a word to a global equivalent of it (basically the translation of it in english)
  * @property word The word itself in the respective language
- * @property representable Indicates whether this word is representable by an image/illustration
+ * @property representable Indicates whether this word is representable by an illustration.
  */
 open class Word(
     val parent: String,
     val word: String,
-    val representable: Boolean = false,
+    val representable: Boolean = true,
 )
