c686b34ec6659a346bc34c37833d7e71eaf2ee06
Arthur Araujo <arthur.araujo@tutanota.com>
2024-12-19 13:59:06 -0300

    Add `pronunciation` field and adapt `representable`

--- src/main/kotlin/cc/wordview/gengolex/languages/Word.kt
+++ src/main/kotlin/cc/wordview/gengolex/languages/Word.kt
@@ -6,9 +6,11 @@ package cc.wordview.gengolex.languages
  * @property parent Identifier used to associate a word to a global equivalent of it (basically the translation of it in english)
  * @property word The word itself in the respective language
  * @property representable Indicates whether this word is representable by an illustration.
+ * @property pronunciation How the word should be spelled. Sometimes a word like '心' needs to be spelled as 'kokoro' instead of 'shin' causing some problems when using TTS.
  */
 open class Word(
     val parent: String,
     val word: String,
     val representable: Boolean = true,
+    val pronunciation: String? = null,
 )
