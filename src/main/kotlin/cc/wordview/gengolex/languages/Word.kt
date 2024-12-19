282326052d6f22fe0433d4bdf7af975495afb9bd
Arthur Araujo <arthur.araujo@tutanota.com>
2024-12-19 14:42:47 -0300

    Revert "#1 Make words representable by default"
    This reverts commit 9ebe8f11

--- src/main/kotlin/cc/wordview/gengolex/languages/Word.kt
+++ src/main/kotlin/cc/wordview/gengolex/languages/Word.kt
@@ -5,12 +5,10 @@ package cc.wordview.gengolex.languages
  *
  * @property parent Identifier used to associate a word to a global equivalent of it (basically the translation of it in english)
  * @property word The word itself in the respective language
- * @property representable Indicates whether this word is representable by an illustration.
- * @property pronunciation How the word should be spelled. Sometimes a word like '心' needs to be spelled as 'kokoro' instead of 'shin' causing some problems when using TTS.
+ * @property representable Indicates whether this word is representable by an image/illustration
  */
 open class Word(
     val parent: String,
     val word: String,
-    val representable: Boolean = true,
-    val pronunciation: String? = null,
+    val representable: Boolean = false,
 )
